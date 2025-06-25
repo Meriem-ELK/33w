b911d5300aab8adbe1b6a93b75407a06b5ec51b8
Meriem ELK <m.elkouarir@gmail.com>
2025-06-25 10:45:34 -0400

    Compléter les modules category et single

--- front-page.php
+++ front-page.php
@@ -14,40 +14,7 @@
 
 <!-- Hero Galerie -->
  <section class="galerie">
-<?= get_template_part("gabarit/galerie"); ?>
-    <!-- <h2 class="galerie__titre">Nos destinations favorites</h2>
-    <div class="galerie__grid">
-        <div class="galerie__element">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/galerie/galerie1.jpg" alt="destination1" class="galerie__image">
-        </div>
-        <div class="galerie__element">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/galerie/galerie2.jpg" alt="destination2" class="galerie__image">
-        </div>
-        <div class="galerie__element">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/galerie/galerie3.jpg" alt="destination3" class="galerie__image">
-        </div>
-        <div class="galerie__element">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/galerie/galerie4.jpg" alt="destination4" class="galerie__image">
-        </div>
-        <div class="galerie__element">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/galerie/galerie5.jpg" alt="destination5" class="galerie__image">
-        </div>
-        <div class="galerie__element">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/galerie/galerie6.jpg" alt="destination6" class="galerie__image">
-        </div>
-        <div class="galerie__element">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/galerie/galerie7.jpg" alt="destination7" class="galerie__image">
-        </div>
-        <div class="galerie__element">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/galerie/galerie8.jpg" alt="destination8" class="galerie__image">
-        </div>
-        <div class="galerie__element">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/galerie/galerie9.jpg" alt="destination9" class="galerie__image">
-        </div>
-        <div class="galerie__element">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/galerie/galerie10.jpg" alt="destination10" class="galerie__image">
-        </div>
-    </div> -->
+    <?= get_template_part("gabarit/galerie"); ?>
 </section> 
 
 <!-- Section populaire -->
--- search.php
+++ search.php
@@ -39,13 +39,25 @@
                 <span class="post__date">
                     <i class="icon-calendar"></i>
                     <?php echo get_the_date('d F Y'); ?>
-                </span> - 
-                <?php if (has_category()) : ?>
-                    <span class="post__category">
-                        <i class="icon-tag"></i>
-                        <?php the_category(', '); ?>
-                    </span>
-                <?php endif; ?>
+                </span> 
+
+                <!-- Afficher catégorie-->
+                <?php
+                    $categories = get_the_category();
+                    $child_cats = [];
+
+                    foreach ( $categories as $cat ) {
+                        if ( $cat->category_parent != 0 ) {
+                            $child_cats[] = $cat->name;
+                        }
+                    }
+
+                    if ( !empty($child_cats) ) : ?>
+                        <div class="category__categories">
+                            <i class="fas fa-folder"></i>
+                            <?php echo implode(', ', $child_cats); ?>
+                        </div>
+            <?php endif; ?>
             </div>
 
             <!-- -->        
