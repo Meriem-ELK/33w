da0e4e8affeda512dcec8b817c5106f7f7a1933a
Meriem ELK <m.elkouarir@gmail.com>
2025-08-22 09:28:44 -0400

    Ajouter l'ancienne et la nouvelle erreur-404 dans le dossier gabarit

--- gabarit/erreur-404.php
+++ gabarit/erreur-404.php
@@ -37,14 +37,7 @@ $message_404 = get_theme_mod('erreur_404_message', 'Pas de panique, cher membre 
             <div class="erreur-404__destinations">
                 <h3 class="erreur-404__destinations-titre">Ou explorez nos destinations :</h3>
                 <nav class="erreur-404__nav">
-                    <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'menu_404_destinations',
-                        'menu_class' => 'erreur-404__menu',
-                        'container' => false,
-                        'fallback_cb' => 'erreur_404_menu_fallback'
-                    ));
-                    ?>
+                   
                 </nav>
             </div>
             
