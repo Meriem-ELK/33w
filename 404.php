3456d68e49a18107695784f23a8ca933b5ecc575
Meriem ELK <m.elkouarir@gmail.com>
2025-08-22 09:36:01 -0400

    Utiliser le nouvelle gabarit erreur-404 dans le modele 404.php

--- 404.php
+++ 404.php
@@ -1,21 +1,12 @@
 <?php
-
 /**
- * le modèle index
- * Représente le modèle par défaut
+ * Le modèle 404
  */
 
-?>
+get_header(); ?>
 
-<?php get_header() ?>
-<div>
-   <section class="error-page">
-        <h1 class="error-page__number">404</h1>
-        <h2 class="error-page__title">L'adresse que vous demandez n'existe pas</h2>
-        <div class="error-page__message">
-            <p>Il se peut que la page ait été déplacée, supprimée ou que l'URL soit incorrecte.</p>
-        </div>
-        <a href="<?php echo home_url(); ?>" class="error-page__button"> Retour à l'accueil</a>
-    </section>
+<div class="page-404">
+    <?php get_template_part('gabarit/erreur-404'); ?>
 </div>
-<?php get_footer();
+
+<?php get_footer(); ?>
